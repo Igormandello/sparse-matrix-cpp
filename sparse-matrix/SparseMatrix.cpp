39435c64c85e2873c7093a862dc72e2623ba6b99
Igor Mandello <igormandello@gmail.com>
2018-09-27 22:36:04 -0300

    Declares and initializes the rows tree

--- sparse-matrix/SparseMatrix.cpp
+++ sparse-matrix/SparseMatrix.cpp
@@ -1,6 +1,8 @@
 template <class T>
 SparseMatrix<T>::SparseMatrix() {
-
+    //Seems confuse but it's just a tree, which each node stores a class with index(row) and data(another tree)
+    //The internal tree have nodes storing a index(col) and data(the data in the pair row/col)
+    this->rows = new AVLTree<IndexedData<AVLTree<IndexedData<T>>>>();
 }
 
 template <class T>
--- sparse-matrix/SparseMatrix.hpp
+++ sparse-matrix/SparseMatrix.hpp
@@ -2,7 +2,8 @@
 #define SPARSE_MATRIX_INCLUDED
 
 #include <iostream>
-#include "../avl-tree-cpp/avl-tree/AVLTree.hpp";
+#include "../avl-tree-cpp/avl-tree/AVLTree.hpp"
+#include "IndexedData.hpp"
 
 using namespace std;
 
@@ -14,6 +15,9 @@ class SparseMatrix {
         void remove(T);
         T get(int, int);
         bool contains(T);
+
+    private:
+        AVLTree<IndexedData<AVLTree<IndexedData<T>>>>* rows;
 };
 
 #include "SparseMatrix.cpp"
